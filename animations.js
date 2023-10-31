c2fab2201e78cd3b3b6c6a87798028e17028246f
Anthony <anthonylsa12@gmail.com>
2023-10-30 21:40:21 -0700

    added js

---
+++ animations.js
@@ -0,0 +1,15 @@
+var foodTruckIcon = $(".food-truck-icon");
+
+foodTruckIcon.delay(300).fadeIn(3000);
+
+var quarterlyMenu = $(".quarterly-menu");
+
+quarterlyMenu.delay(300).fadeIn(3000);
+
+var mathematicianFavorites = $(".mathematician-favorites");
+
+mathematicianFavorites.delay(300).fadeIn(3000);
+
+var locationAndHours = $(".location-and-hours");
+
+locationAndHours.delay(300).fadeIn(3000)
